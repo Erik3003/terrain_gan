5d2123a457fe88b1ac74972532cf6a9e46f93077
Erik3003 <erik.t.3003@googlemail.com>
2021-09-10 20:28:43 +0200

    Added comments

--- gen_data.py
+++ gen_data.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import tensorflow as tf
 import cv2
-import numpy as np
 
 IMG_SHAPE = (256, 256, 1)
 
-
+# Filter images unfit for model training
+# src: https://github.com/dandrino/terrain-erosion-3-ways/blob/58cdebf74bf9b0b08de61187fbd0ecebd4bb2386/generate_training_images.py#L16
 def clean_sample(sample):
     # Only accept values that span a given range. This is to capture more
     # mountainous samples.
@@ -22,15 +21,17 @@ def clean_sample(sample):
 
     return sample
 
-
+# Save the training images
 def save_img(img, number):
     cv2.imwrite("data\\img\\" + str(number) + ".png", img)
 
-
+# Process the images
 def process_img(sample, number):
+    # Check if it is a good sample, otherwise return and not save image
     sample = clean_sample(sample)
     if sample is None:
         return 0
+    # Transform image through rotation and flipping to obtain and save 8 training images
     save_img(sample, number)
     save_img(cv2.rotate(sample, cv2.ROTATE_90_CLOCKWISE), number + 1)
     save_img(cv2.rotate(sample, cv2.ROTATE_90_COUNTERCLOCKWISE), number + 2)
@@ -42,10 +43,12 @@ def process_img(sample, number):
     save_img(cv2.rotate(sample, cv2.ROTATE_180), number + 7)
     return 8
 
-
+# Counts the number of saved images
 counter = 0
+# Paths to unprocessed images
 paths = []
 
+# Iterate to build the paths of the training data
 for i in range(1, 5001):
     path = str(i)
     while len(path) < 4:
@@ -53,9 +56,11 @@ for i in range(1, 5001):
     path = "data\\" + path + "_h.png"
     paths.append(path)
 
+# Iterates over the paths and loads images as grayscale
 for path in paths:
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
+    # Cut out nine 256 by 256 subimages and processes them
     image_train = image[:256, :256]
     counter += process_img(image_train, counter)
 
--- wgan_gp.py
+++ wgan_gp.py
@@ -12,6 +12,7 @@ import gc
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
+# Define growth of resolutions
 img_shape_config = [
     (4, 4, 1),
     (8, 8, 1),
@@ -22,61 +23,79 @@ img_shape_config = [
     (256, 256, 1)
 ]
 
+# Current resolution
 IMG_SHAPE = img_shape_config[0]
+# Epochs per resolution
 EPOCHS_PER_SIZE = 50
+# Total number of epochs (when using progressive growth)
 TOTAL_EPOCHS = EPOCHS_PER_SIZE * ((len(img_shape_config)-1) * 2 + 1)
+# Size of Minibatches
 BATCH_SIZE = 32
+# Discriminator training runs per generator training run
 N_DISCRIMINATOR = 5
-CURRENT_SIZE = 0
+# Current state of transition. 1.0 in stabilization phases
 CURRENT_TRANSITION = tf.Variable(1.0, trainable=False)
+# Transition growth per minibatch, gets calculated automatically
 TRANSITION_SPEED = 0.0
+# Returns True when in transition phase
 IS_TRANSITION = False
+# Enables progressive growth when True
 DO_GROWTH = False
+# Saves models after each epoch when True
 DO_SAVE = True
 
+# Size/Dimension of the latent noise of the generator
 noise_dim = 512
 
+# When not progressively growing, set resolution
 if not DO_GROWTH:
     IMG_SHAPE = (256, 256, 1)
 
 
+# Method for loading images from storage into numpy arrays for training
+# res is a int and determines the resolution of the loaded images
 def get_images(res):
     images = []
+    # Iterate over 86176 training images
     for index in range(86176):
+        # Build path and load the image as a grayscale image
         image = cv2.imread("data\\img\\" + str(index) + ".png", cv2.IMREAD_GRAYSCALE)
+        # Resize image when needed
         if res != 256:
             image = cv2.resize(image, (res, res))
+        # Add images to list
         images.append(image)
-        """images.append(cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE))
-        images.append(cv2.rotate(image, cv2.ROTATE_180))
-        images.append(cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE))
-        image = cv2.flip(image, 1)
-        images.append(image)
-        images.append(cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE))
-        images.append(cv2.rotate(image, cv2.ROTATE_180))
-        images.append(cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE))"""
         del image
 
+    # Convert list to array
     images = np.array(images, dtype="float32")
+    # Reshape array for training
     images = images.reshape(images.shape[0], *(res, res, 1))
+    # Print shape
     print(images.shape)
+    # Scale images for training, so the generator trains for outputs between -1 and 1
     images = (images - 127.5) / 127.5
+    # Shuffle images
     np.random.seed(3213)
     np.random.shuffle(images)
     return images
 
-
+# Load images
 train_images = get_images(IMG_SHAPE[0])
 
-
+# Pixel normalization "layer"
+# src: https://github.com/tkarras/progressive_growing_of_gans/blob/6c2a3add95ce0d12e0df089cacbb3d4a56cec019/networks.py#L120
 def pixel_norm(x, epsilon=1e-8):
     return x * tf.math.rsqrt(tf.reduce_mean(tf.square(x), axis=1, keepdims=True) + epsilon)
 
-
+# Linear interpolation function, transitions between two tensors based on weight t
+# src: https://github.com/tkarras/progressive_growing_of_gans/blob/6c2a3add95ce0d12e0df089cacbb3d4a56cec019/networks.py#L15
 def lerp(a, b, t):
     return a + (b - a) * t
 
-
+# Equalized Convolutional Layer
+# Scales weights (i.e. the kernel) with he constant
+# src: https://github.com/p4vv37/3dml/blob/d0096bca0c487de2b4408d4ac6807ac3576fea9d/5_generative_networks/progan/layers.py#L111
 class EqualizedConv2D(layers.Conv2D):
     def __init__(self, *args, **kwargs):
         self.scale = 1.0
@@ -99,7 +118,9 @@ class EqualizedConv2D(layers.Conv2D):
             return self.activation(outputs)
         return outputs
 
-
+# Equalized Dense Layer
+# Same effect as EqualizedConv2D for dense layer
+# Functionality gets disabled when not progressively growing
 class EqualizedDense(layers.Dense):
     def __init__(self, *args, gain=1, **kwargs):
         self.scale = 1.0
@@ -122,7 +143,8 @@ class EqualizedDense(layers.Dense):
             return self.activation(outputs)
         return outputs
 
-
+# Minibatch standard deviation layer
+# src: https://github.com/p4vv37/3dml/blob/d0096bca0c487de2b4408d4ac6807ac3576fea9d/5_generative_networks/progan/layers.py#L35
 class MinibatchStdev(layers.Layer):
     def __init__(self, **kwargs):
         super(MinibatchStdev, self).__init__(**kwargs)
@@ -144,14 +166,15 @@ class MinibatchStdev(layers.Layer):
         input_shape[-1] += 1
         return tuple(input_shape)
 
-
+# Initializer for Equalized Learning
 initializer = tf.keras.initializers.RandomNormal(0, 1)
 
+# Change initializer and batch size when not using progressive growth
 if not DO_GROWTH:
     initializer = tf.keras.initializers.GlorotUniform()
     BATCH_SIZE = 16
 
-
+# Convolutional/Downsampling block of discriminator
 def conv_block(x, depth, depth_multiplier=2, weights=None, downsample=True):
     l_1 = EqualizedConv2D(depth, kernel_size=3, kernel_initializer=initializer, strides=1, padding="same")
     x = l_1(x)
@@ -168,7 +191,7 @@ def conv_block(x, depth, depth_multiplier=2, weights=None, downsample=True):
         x = layers.AveragePooling2D(2)(x)
     return x
 
-
+# Transition block of discriminator
 def d_transition_block(x, depth, depth_multiplier=2, weights=None):
     y = layers.AveragePooling2D(2)(x)
     old_exit = EqualizedConv2D(depth * depth_multiplier, kernel_size=1, kernel_initializer=initializer, strides=1, padding="same", trainable=False)
@@ -180,11 +203,11 @@ def d_transition_block(x, depth, depth_multiplier=2, weights=None):
     x = EqualizedConv2D(depth, kernel_size=1, kernel_initializer=initializer, strides=1, padding="same")(x)
     x = layers.LeakyReLU(0.2)(x)
     x = conv_block(x, depth, depth_multiplier)
-
+    # Interpolate between old and new output
     x = lerp(y, x, CURRENT_TRANSITION)
     return x
 
-
+# Filter sizes of convolutional blocks of the discriminator
 d_layer_config = [
     [512, 1],
     [512, 1],
@@ -196,9 +219,10 @@ d_layer_config = [
 ]
 
 
+# Discriminator model
 def get_discriminator_model():
     img_input = layers.Input(shape=IMG_SHAPE)
-
+    # Build whole network when not progressively growing
     if not DO_GROWTH:
         x = EqualizedConv2D(d_layer_config[-1][0], kernel_size=1, kernel_initializer=initializer, strides=1, padding="same")(
             img_input)
@@ -214,7 +238,7 @@ def get_discriminator_model():
                             padding="same")(
             img_input)
         x = layers.LeakyReLU(0.2)(x)
-
+    # Disable minibatch standard deviation layer when not progressivly growing
     if DO_GROWTH:
         x = MinibatchStdev()(x)
 
@@ -237,6 +261,7 @@ def get_discriminator_model():
     return d_model
 
 
+# Deconvolutional/Upsampling block of the generator
 def deconv_block(x, depth, upsampling=True):
     if upsampling:
         x = layers.UpSampling2D(2, interpolation="nearest")(x)
@@ -248,7 +273,7 @@ def deconv_block(x, depth, upsampling=True):
     x = pixel_norm(x)
     return x
 
-
+# Transition block of the generator
 def g_transition_block(x, depth, weights=None):
     old_exit = EqualizedConv2D(filters=IMG_SHAPE[-1], kernel_size=1, kernel_initializer=initializer, strides=1, padding="same", trainable=False)
     y = old_exit(x)
@@ -260,11 +285,11 @@ def g_transition_block(x, depth, weights=None):
     x = deconv_block(x, depth)
     x = EqualizedConv2D(filters=IMG_SHAPE[-1], kernel_size=1, kernel_initializer=initializer, strides=1, padding="same")(x)
     x = layers.Activation("linear")(x)
-
+    # Interpolate between old and new output
     x = lerp(y, x, CURRENT_TRANSITION)
     return x
 
-
+# Filter sizes of deconvolutional blocks of the generator
 g_layer_config = [
     [512],
     [512],
@@ -275,7 +300,7 @@ g_layer_config = [
     [16]
 ]
 
-
+# Generator model
 def get_generator_model():
     noise = layers.Input(shape=(noise_dim,))
     if DO_GROWTH:
@@ -286,6 +311,7 @@ def get_generator_model():
     x = layers.Reshape((4, 4, g_layer_config[0][0]))(x)
 
     x = deconv_block(x, *g_layer_config[0], upsampling=False)
+    # Build whole model when not progressively growing
     if not DO_GROWTH:
         x = deconv_block(x, *g_layer_config[1])
         x = deconv_block(x, *g_layer_config[2])
@@ -300,24 +326,25 @@ def get_generator_model():
     g_model = keras.models.Model(noise, x, name="generator")
     return g_model
 
-
+# Build models and prints them to the console
 if __name__ == "__main__":
     d_model = get_discriminator_model()
     d_model.summary()
     g_model = get_generator_model()
     g_model.summary()
 
-
+# Wasserstein loss function for discriminator
 def discriminator_loss(real_img, fake_img):
     real_loss = tf.reduce_mean(real_img)
     fake_loss = tf.reduce_mean(fake_img)
     return fake_loss - real_loss
 
-
+# Generator loss function
 def generator_loss(fake_img):
     return -tf.reduce_mean(fake_img)
 
-
+# Wasserstein GAN with gradient penalty training model
+# src: https://github.com/keras-team/keras-io/blob/master/examples/generative/ipynb/wgan_gp.ipynb
 class WGAN(keras.Model):
     def __init__(
             self,
@@ -334,6 +361,7 @@ class WGAN(keras.Model):
         self.d_steps = discriminator_extra_steps
         self.gp_weight = gp_weight
 
+    # Compiles the models
     def compile(self, d_optimizer, g_optimizer, d_loss_fn, g_loss_fn):
         super(WGAN, self).compile()
         self.d_optimizer = d_optimizer
@@ -341,6 +369,7 @@ class WGAN(keras.Model):
         self.d_loss_fn = d_loss_fn
         self.g_loss_fn = g_loss_fn
 
+    # Calculates the gradient penalty
     def gradient_penalty(self, batch_size, real_images, fake_images):
         alpha = tf.random.normal([batch_size, 1, 1, 1], 0.0, 1.0)
         diff = fake_images - real_images
@@ -355,17 +384,21 @@ class WGAN(keras.Model):
         gp = tf.reduce_mean((norm - 1.0) ** 2)
         return gp
 
+    # Training implementation
     def train_step(self, real_images):
         if isinstance(real_images, tuple):
             real_images = real_images[0]
 
         batch_size = tf.shape(real_images)[0]
 
+        # When in transition phase, interpolates the images with upscaled lower resolution ones,
+        # to smoothly scale the output resolution
         if IS_TRANSITION:
             small_images = tf.image.resize(real_images, (int(IMG_SHAPE[0]/2), int(IMG_SHAPE[1]/2)))
             small_images = tf.image.resize(small_images, IMG_SHAPE[:2], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
             real_images = lerp(small_images, real_images, CURRENT_TRANSITION)
 
+        # Train the discriminator N_DISCRIMINATOR times
         for i in range(self.d_steps):
             random_latent_vectors = tf.random.normal(
                 shape=(batch_size, self.latent_dim)
@@ -375,23 +408,29 @@ class WGAN(keras.Model):
                 fake_logits = self.discriminator(fake_images, training=True)
                 real_logits = self.discriminator(real_images, training=True)
 
+                # Calculate Wasserstein loss
                 d_cost = self.d_loss_fn(real_img=real_logits, fake_img=fake_logits)
+                # Calculate and add gradient penalty
                 gp = self.gradient_penalty(batch_size, real_images, fake_images)
                 d_loss = d_cost + gp * self.gp_weight
+                # Calculae and add epsilon penalty
                 epsilon_penalty = tf.square(real_logits)
                 d_loss += tf.reduce_mean(epsilon_penalty) * 0.001
 
+            # Apply gradients to discriminator
             d_gradient = tape.gradient(d_loss, self.discriminator.trainable_variables)
             self.d_optimizer.apply_gradients(
                 zip(d_gradient, self.discriminator.trainable_variables)
             )
 
+        # Generate random latent noise
         random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
         with tf.GradientTape() as tape:
             generated_images = self.generator(random_latent_vectors, training=True)
             gen_img_logits = self.discriminator(generated_images, training=True)
+            # Generate generator loss
             g_loss = self.g_loss_fn(gen_img_logits)
-
+        # Apply gradients to generator
         gen_gradient = tape.gradient(g_loss, self.generator.trainable_variables)
         self.g_optimizer.apply_gradients(
             zip(gen_gradient, self.generator.trainable_variables)
@@ -399,6 +438,7 @@ class WGAN(keras.Model):
         return {"d_loss": d_loss, "g_loss": g_loss}
 
 
+# Callback class, handles growth and images
 class GANMonitor(keras.callbacks.Callback):
 
     def __init__(self, num_img=6, latent_dim=128):
@@ -407,46 +447,56 @@ class GANMonitor(keras.callbacks.Callback):
         self.random_latent_vectors = tf.random.normal(shape=(self.num_img, self.latent_dim), seed=3342934)
         self.stage = 1
 
+    # Call to set to next size stage
     def next(self):
         self.stage += 1
 
+    # Progressively grows CURRENT_TRANSITON when in transition phase
     def on_train_batch_end(self, step, logs=None):
         if IS_TRANSITION:
             CURRENT_TRANSITION.assign_add(TRANSITION_SPEED)
             if CURRENT_TRANSITION > 1.0:
                 CURRENT_TRANSITION.assign(1.0)
 
+    # Creates images and changes networks after the last epoch of a phase
     def on_epoch_end(self, epoch, logs=None):
         global IS_TRANSITION
         generated_images = self.model.generator(self.random_latent_vectors)
         generated_images = (generated_images * 127.5) + 127.5
         current_epoch = epoch + (self.stage - 1) * EPOCHS_PER_SIZE
 
+        # Generates and saves images to allow track generator learning process
         for i in range(self.num_img):
             img = generated_images[i].numpy()
             img = keras.preprocessing.image.array_to_img(img)
             img.save("output\\{epoch}_heightmap_{i}.png".format(i=i, epoch=current_epoch))
 
+        # Save models, only possible when not in transition phase
         if DO_SAVE and not IS_TRANSITION:
             self.model.generator.save("models\\Generator_{epoch}.h5".format(epoch=current_epoch))
             self.model.discriminator.save("models\\Discriminator.h5")
 
+        # Do not change networks when not progressively growing
         if not DO_GROWTH:
             return
 
+        # Return when first epoch, except when EPOCH_PER_SIZE is 1
         if epoch == 0 and EPOCHS_PER_SIZE != 1:
             return
 
+        # If last epoch in phase, change networks
         if epoch == 0 or epoch % (EPOCHS_PER_SIZE - 1) == 0:
             current_network = int(math.ceil(self.stage / 2))
             print("Changing networks on stage " + str(current_network))
 
+            # Change networks to stabilization phase
             if self.stage % 2 == 0:
                 print("Changing to non-transition")
                 IS_TRANSITION = False
                 CURRENT_TRANSITION.assign(1.0)
                 self.transition_discriminator(current_network)
                 self.transition_generator(current_network)
+            # Change networks to transition phase
             else:
                 print("Changing to transition")
                 IS_TRANSITION = True
@@ -456,16 +506,19 @@ class GANMonitor(keras.callbacks.Callback):
                 self.extend_discriminator(current_network)
                 self.extend_generator(current_network)
 
+    # Changes generator to transition phase by adding transition block
     def extend_generator(self, config):
         config = g_layer_config[config]
         weights = self.model.generator.layers[-2].get_weights()
         self.model.generator = keras.models.Model(self.model.generator.input, g_transition_block(self.model.generator.layers[-3].output, *config, weights=weights), name="new_generator")
         self.model.generator.summary()
 
+    # Changes generator from transition phase to stabilization phase by removing unneeded old layers
     def transition_generator(self, config):
         self.model.generator = keras.models.Model(self.model.generator.input, self.model.generator.layers[-4].output, name="new_generator")
         self.model.generator.summary()
 
+    # Changes discriminator to discriminator phase by adding transition block
     def extend_discriminator(self, config):
         config = d_layer_config[config]
         weights = self.model.discriminator.layers[1].get_weights()
@@ -479,6 +532,7 @@ class GANMonitor(keras.callbacks.Callback):
         self.model.discriminator = keras.models.Model(input_layer, x, name="new_discriminator")
         self.model.discriminator.summary()
 
+    # Changes discriminator from transition phase to stabilization phase by removing unneeded old layers
     def transition_discriminator(self, config):
         config = d_layer_config[config]
         d_layers = self.model.discriminator.layers[14:]
@@ -500,17 +554,21 @@ class GANMonitor(keras.callbacks.Callback):
         self.model.discriminator.summary()
 
 
+# Optimizer for generator
 generator_optimizer = keras.optimizers.Adam(
     learning_rate=0.0001, beta_1=0.0, beta_2=0.99, epsilon=1e-8
 )
 
+# Optimizer for discriminator
 discriminator_optimizer = keras.optimizers.Adam(
     learning_rate=0.0001, beta_1=0.0, beta_2=0.99, epsilon=1e-8
 )
 
 if __name__ == "__main__":
+    # Create GANMonitor
     cbk = GANMonitor(num_img=1, latent_dim=noise_dim)
 
+    # Create WGAN
     wgan = WGAN(
         discriminator=d_model,
         generator=g_model,
@@ -518,6 +576,7 @@ if __name__ == "__main__":
         discriminator_extra_steps=N_DISCRIMINATOR
     )
 
+    # Compile WGAN
     wgan.compile(
         d_optimizer=discriminator_optimizer,
         g_optimizer=generator_optimizer,
@@ -525,23 +584,32 @@ if __name__ == "__main__":
         d_loss_fn=discriminator_loss
     )
 
+    #Calculate steps per epoch and TRANSITION_SPEED
     steps_per_epoch = int(math.ceil(len(train_images) / BATCH_SIZE))
     TRANSITION_SPEED = 1 / (steps_per_epoch * EPOCHS_PER_SIZE)
 
+    # When not using progressive growth, fit model once
     if not DO_GROWTH:
         wgan.fit(train_images, batch_size=BATCH_SIZE, epochs=EPOCHS_PER_SIZE, callbacks=[cbk])
+    # When using progressive growth, each phase gets own fit call
+    # Otherwise the model will not be able to train correctly
     else:
         # Start training the model.
         for i in range(int(TOTAL_EPOCHS / EPOCHS_PER_SIZE)):
             if IS_TRANSITION:
+                # Reload images in higher resolution
                 del train_images
                 gc.collect()
                 train_images = get_images(IMG_SHAPE[0])
+                # Lower BATCH_SIZE to not exceed vram limitations on higher resolutions
                 if cbk.stage == 12 or not DO_GROWTH:
                     BATCH_SIZE = 8
+                # Recalculate TRANSITION speed for new batchsize
                 steps_per_epoch = int(math.ceil(len(train_images) / BATCH_SIZE))
+                TRANSITION_SPEED = 1 / (steps_per_epoch * EPOCHS_PER_SIZE)
             wgan.fit(train_images, batch_size=BATCH_SIZE, epochs=EPOCHS_PER_SIZE, callbacks=[cbk])
 
+            # Model needs to be recompiled to be able to train the added layers
             wgan = WGAN(discriminator=wgan.discriminator, generator=wgan.generator, latent_dim=noise_dim, discriminator_extra_steps=N_DISCRIMINATOR)
             wgan.compile(
                 d_optimizer=discriminator_optimizer,
